5bf23bc7b2bcb514a27b7310849cced91f0e963e
Quiyet Brul <qbrul0514@gmail.com>
2023-10-27 13:53:26 -0600

    Time: 2 ms (49.25%), Space: 6.3 MB (23.79%) - LeetHub

---
+++ 1518-water-bottles/1518-water-bottles.cpp
@@ -0,0 +1,6 @@
+class Solution {
+public:
+    int numWaterBottles(int numBottles, int numExchange) {
+        return numBottles < numExchange ? numBottles : numExchange + numWaterBottles(numBottles - numExchange + 1, numExchange);
+    }
+};
