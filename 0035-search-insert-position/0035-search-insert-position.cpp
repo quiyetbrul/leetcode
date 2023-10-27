df27d1f899b55afb0cb2edd2e618d6ce88dc6d90
Quiyet Brul <qbrul0514@gmail.com>
2023-10-27 13:46:59 -0600

    Time: 3 ms (73.72%), Space: 10 MB (24.00%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        
+        vector<int>::iterator it = lower_bound(nums.begin(), nums.end(), target); 
+        return (it-nums.begin());
+    }
+};
