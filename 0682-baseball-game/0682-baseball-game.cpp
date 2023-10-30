d5e1a4bfd9cda90a58f6bfb1443849a0cf07f96e
Quiyet Brul <qbrul0514@gmail.com>
2023-10-29 20:24:57 -0600

    Time: 0 ms (100%), Space: 8.65 MB (50.12%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.cpp
+++ 0682-baseball-game/0682-baseball-game.cpp
@@ -1,32 +1,56 @@
 class Solution {
 public:
-    int calPoints(vector<string>& operations) {
-    std::vector<int> record;
-        
-    for (auto i : operations) {
-        if (i == "C") {
-            if (!record.empty()) {
-                record.pop_back();
-            }
-        } else if (i == "D") {
-            if (!record.empty()) {
-                record.push_back(record.back() * 2);
-            }
-        } else if (i == "+") {
-            if (record.size() >= 2) {
-                record.push_back(record[record.size() - 1] + record[record.size() - 2]);
-            }
-        } else {
-            try {
-                record.push_back(std::stoi(i));
-            } catch (const std::invalid_argument& e) {
-                std::cerr << "Invalid input: " << i << std::endl;
-            }
+  int calPoints(std::vector<std::string> &ops) {
+    //// VECTOR
+    // std::vector<int> record;
+    // for (auto i : operations) {
+    //     if (i == "C") {
+    //         if (!record.empty()) {
+    //             record.pop_back();
+    //         }
+    //     } else if (i == "D") {
+    //         if (!record.empty()) {
+    //             record.push_back(record.back() * 2);
+    //         }
+    //     } else if (i == "+") {
+    //         if (record.size() >= 2) {
+    //             record.push_back(record.back() + record[record.size() - 2]);
+    //         }
+    //     } else {
+    //         try {
+    //             record.push_back(std::stoi(i));
+    //         } catch (const std::invalid_argument& e) {
+    //             std::cerr << "Invalid input: " << i << std::endl;
+    //         }
+    //     }
+    // }
+    // return std::accumulate(record.begin(), record.end(), 0);
 
-        }
+    //// OR STACK BASED
+    int value1;
+    int value2;
+    int ans = 0;
+    std::stack<int> stk;
+
+    for (std::string i : ops) {
+      if (i == "C") {
+        stk.pop();
+      } else if (i == "D") {
+        stk.push(stk.top() * 2);
+      } else if (i == "+") {
+        value1 = stk.top();
+        stk.pop();
+        value2 = stk.top();
+        stk.push(value1);
+        stk.push(value1 + value2);
+      } else {
+        stk.push(stoi(i)); // stoi() to convert string into integer
+      }
     }
-    
-    return std::accumulate(record.begin(), record.end(), 0);
-}
-
+    while (!stk.empty()) {
+      ans += stk.top();
+      stk.pop();
+    }
+    return ans;
+  }
 };
