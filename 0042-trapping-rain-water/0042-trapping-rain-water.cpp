22ab68f77244b01cf5e630309c4e2b029cb0b16f
Quiyet Brul <qbrul0514@gmail.com>
2023-10-31 23:06:44 -0600

    Time: 11 ms (64.84%), Space: 20 MB (70.40%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,6 +1,5 @@
 class Solution {
 public:
-    //total water is trapped into the bars
     int trap(vector<int>& h) {
         int l=0,r=h.size()-1,lmax=INT_MIN,rmax=INT_MIN,ans=0;
         while(l<r){
