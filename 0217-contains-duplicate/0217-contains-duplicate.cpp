9e9c89130775392403259661333d6a1d10b535b3
Quiyet Brul <qbrul0514@gmail.com>
2023-11-19 17:33:19 -0700

    Time: 153 ms (6.01%), Space: 73.7 MB (6.06%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -9,4 +9,27 @@ bool containsDuplicate(std::vector<int> &nums) {
 
   return s.size() != nums.size();
 }
+    
+bool containsDuplicateUnorderedMap(std::vector<int> &nums) {
+  if (nums.size() > 1) {
+    std::unordered_map<int, int> numsMap;
+    for (const auto &i : nums) {
+      if (numsMap[i])
+        return true;
+      numsMap[i] = 1;
+    }
+  }
+  return false;
+}
+    
+bool containsDuplicateSort(std::vector<int> &nums) {
+  sort(nums.begin(), nums.end());
+  int n = nums.size();
+  for (int i = 1; i < n; i++) {
+    if (nums[i] == nums[i - 1])
+      return true;
+  }
+  return false;
+}
+
 };
