5ee183ef965c2065826af578fec44c6da4ddcfe4
Quiyet Brul <qbrul0514@gmail.com>
2023-11-28 21:25:02 -0700

    Time: 0 ms (100.00%), Space: 9.8 MB (91.68%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -4,6 +4,8 @@ public:
         // check last element of each row
         // if target >= row[length-1]
             // move to the next row (top to bottom)
+                // check if it makes sense to go to the next row
+                    // check target > row[i]col[length-1] && target < row[i+1]col[0] to return false earlier
         // else
             // iterate through each column in the same row (right to left)
         
@@ -21,9 +23,8 @@ public:
                 return true;
             }
             if (target > cur) {
-                // Check if it's worthwhile to move to the next row
                 if (row < rows - 1 && target < matrix[row + 1][0]) {
-                    return false; // Target is not in matrix
+                    return false;
                 }
                 row++;
             } else {
