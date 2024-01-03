5a364f16a8e9a4379da496be63f956b12cd41c6e
Quiyet Brul <qbrul0514@gmail.com>
2024-01-03 14:18:15 -0700

    Time: 83 ms (83.23%), Space: 23.5 MB (72.49%) - LeetHub

---
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int countOnes(const string& str) {
+        int ones = 0;
+        for (char c : str) {
+            if (c == '1') {
+                ones++;
+            }
+        }
+        return ones;
+    }
+
+    int numberOfBeams(vector<string>& bank) {
+        int total = 0;
+        int previous = 0;
+        
+        for(const std::string& row : bank){
+            int current = countOnes(row);
+            if(current == 0) continue;
+            total += (current*previous);
+            previous = current;
+        }
+        
+        return total;
+    }
+};
