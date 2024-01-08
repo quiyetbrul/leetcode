5831f77e725b5b8c5a321ab8cdd93b736d387488
Quiyet Brul <qbrul0514@gmail.com>
2024-01-07 20:05:53 -0700

    Time: 48 ms (90.77%), Space: 32.5 MB (66.64%) - LeetHub

--- 1200-minimum-absolute-difference/1200-minimum-absolute-difference.cpp
+++ 1200-minimum-absolute-difference/1200-minimum-absolute-difference.cpp
@@ -4,15 +4,18 @@ public:
         std::sort(arr.begin(), arr.end());
 
         std::vector<std::vector<int>> result;
-
         int min_diff = INT_MAX;
-        for(int i = 1; i < arr.size(); i++)
-            min_diff = std::min(arr[i]-arr[i-1], min_diff);
-        
-        for(int i = 1; i < arr.size(); i++)
-            if(arr[i] - arr[i-1] == min_diff){
-                result.push_back({arr[i-1], arr[i]});
+
+        for (int i = 1; i < arr.size(); i++) {
+            int diff = arr[i] - arr[i - 1];
+            if (diff < min_diff) {
+                min_diff = diff;
+                result.clear();
+                result.push_back({arr[i - 1], arr[i]});
+            } else if (diff == min_diff) {
+                result.push_back({arr[i - 1], arr[i]});
             }
+        }
 
         return result;
     }
