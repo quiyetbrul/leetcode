2fda49eadbfdb786cbdd4b3682055c2d192bca72
Quiyet Brul <qbrul0514@gmail.com>
2024-01-08 16:30:13 -0700

    Time: 0 ms (100.00%), Space: 9.8 MB (92.68%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,26 +1,57 @@
+// class Solution {
+// public:
+//     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+//         // first check each row and column sizes
+//         int rows = matrix.size();
+//         int cols = matrix[0].size();
+        
+//         if (rows == 0 || cols == 0) return false;
+        
+//         int row = 0;
+//         int col = cols - 1;
+        
+//         while(row < rows && col > -1){
+//             int current = matrix[row][col];
+//             if(current == target)
+//                 return true;
+
+//             // could be better and make it binary search instead of iterating through each element in the row
+//             if (target > current){
+//                 if(row < rows - 1 && target < matrix[row + 1][0])
+//                     return false;
+//                 row++;
+//             }else 
+//                 col--;
+//         }
+        
+//         return false;
+//     }
+// };
+
+// USING BINARY SEARCH
+
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        // first check each row and column sizes
         int rows = matrix.size();
         int cols = matrix[0].size();
         
         if (rows == 0 || cols == 0) return false;
         
-        int row = 0;
-        int col = cols - 1;
-        
-        while(row < rows && col > -1){
-            int current = matrix[row][col];
-            if(current == target)
-                return true;
-
-            if (target > current){
-                if(row < rows - 1 && target < matrix[row + 1][0])
-                    return false;
-                row++;
-            }else 
-                col--;
+        for (int i = 0; i < rows; ++i) {
+            // Apply binary search within each row
+            int left = 0, right = cols - 1;
+            
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+                
+                if (matrix[i][mid] == target)
+                    return true;
+                else if (matrix[i][mid] < target)
+                    left = mid + 1;
+                else
+                    right = mid - 1;
+            }
         }
         
         return false;
