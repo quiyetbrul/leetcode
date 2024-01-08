ef58807de4252a7c15c78bd0ea6586d22d8d4892
Quiyet Brul <qbrul0514@gmail.com>
2024-01-08 15:19:06 -0700

    Time: 3 ms (75.25%), Space: 10.1 MB (12.08%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,35 +1,26 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        // check last element of each row
-        // if target >= row[length-1]
-            // move to the next row (top to bottom)
-                // check if it makes sense to go to the next row
-                    // check target > row[i]col[length-1] && target < row[i+1]col[0] to return false earlier
-        // else
-            // iterate through each column in the same row (right to left)
-        
+        // first check each row and column sizes
         int rows = matrix.size();
-        if (rows == 0) return false;
-        
         int cols = matrix[0].size();
-        if (cols == 0) return false;
         
-        int row = 0, col = cols - 1;
+        if (rows == 0 || cols == 0) return false;
         
-        while (row < rows && col > -1) {
-            int cur = matrix[row][col];
-            if (cur == target) {
+        int row = 0;
+        int col = cols - 1;
+        
+        while(row < rows && col > -1){
+            int current = matrix[row][col];
+            if(current == target)
                 return true;
-            }
-            if (target > cur) {
-                if (row < rows - 1 && target < matrix[row + 1][0]) {
+
+            if (target > current){
+                if(row < rows - 1 && target < matrix[row + 1][0])
                     return false;
-                }
                 row++;
-            } else {
+            }else 
                 col--;
-            }
         }
         
         return false;
