ce816bac7a398eb9e127c8fc28ef8491f66061aa
Quiyet Brul <qbrul0514@gmail.com>
2024-01-15 15:34:29 -0700

    Time: 34 ms (74.66%), Space: 19.4 MB (22.54%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -6,20 +6,21 @@ public:
         int left = 1;
         int right = piles.back();
         
-        while(left <= right){
+        while (left < right) {
             int mid = left + (right - left) / 2;
-            
-            if (findHours(piles, mid) > h) left = mid + 1;
-            else right = mid - 1;
-            
+            int hours = findHours(piles, mid);
+
+            if (hours > h) left = mid + 1;
+            else right = mid;
         }
         return left;
     }
-    
-    double findHours(const std::vector<int>& piles, int mid){
-        double total = 0;
-        for(const int& pile : piles)
-            total += std::ceil(static_cast<double>(pile) / mid);
+
+private:
+    int findHours(const std::vector<int>& piles, int mid) {
+        int total = 0;
+        for (int pile : piles)
+            total += (pile + mid - 1) / mid;
         return total;
     }
 
