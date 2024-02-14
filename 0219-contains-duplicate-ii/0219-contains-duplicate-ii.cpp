114ff52f5fc61bea7b7a46da489f071237d8347e
Quiyet Brul <qbrul0514@gmail.com>
2024-02-13 21:34:03 -0700

    Time: 121 ms (76.34%), Space: 81 MB (25.51%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     bool containsNearbyDuplicate(std::vector<int>& nums, int k) {       
-        std::unordered_map<int, int> s; // key element, value index
+        std::unordered_map<int, int> m; // key element, value index
         for (int i = 0; i < nums.size(); i++) {
-            auto it = s.find(nums[i]);
-            if (it != s.end() && i - it->second <= k) return true;
-            s[nums[i]] = i;
+            auto it = m.find(nums[i]);
+            if (it != m.end() && i - it->second <= k) return true;
+            m[nums[i]] = i;
         }
         return false;
     }
