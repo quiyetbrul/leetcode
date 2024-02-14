f7976b18c18d73c21fc19b9dbc8db555b539b94f
Quiyet Brul <qbrul0514@gmail.com>
2024-02-13 21:34:57 -0700

    Time: 114 ms (88.85%), Space: 76 MB (83.32%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -1,11 +1,23 @@
 class Solution {
 public:
-    bool containsNearbyDuplicate(std::vector<int>& nums, int k) {       
-        std::unordered_map<int, int> m; // key element, value index
-        for (int i = 0; i < nums.size(); i++) {
-            auto it = m.find(nums[i]);
-            if (it != m.end() && i - it->second <= k) return true;
-            m[nums[i]] = i;
+    // bool containsNearbyDuplicate(std::vector<int>& nums, int k) {       
+    //     std::unordered_map<int, int> m; // key element, value index
+    //     for (int i = 0; i < nums.size(); i++) {
+    //         auto it = m.find(nums[i]);
+    //         if (it != m.end() && i - it->second <= k) return true;
+    //         m[nums[i]] = i;
+    //     }
+    //     return false;
+    // }
+    
+    bool containsNearbyDuplicate(std::vector<int>& nums, int k) {
+        std::unordered_set<int> window;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (i > k) // If window size exceeds 'k', remove the oldest element from the window
+                window.erase(nums[i - k - 1]);
+            if (window.find(nums[i]) != window.end()) // Check if the current element already exists in the window
+                return true;
+            window.insert(nums[i]); // Add the current element to the window
         }
         return false;
     }
