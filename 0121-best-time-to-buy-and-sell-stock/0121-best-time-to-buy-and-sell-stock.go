2503d9505dd1ad8f0d2f2010e0214c2482b880a9
Quiyet Brul <qbrul0514@gmail.com>
2024-02-13 22:55:59 -0700

    Time: 106 ms (57.82%), Space: 8.7 MB (18.19%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.go
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.go
@@ -1,31 +1,6 @@
 // time o(n), space o(1)
-// sliding window algorithm
-func maxProfit(prices []int) int {
-////  init left, right, maxprofit, slice length
-//  left := 0
-//  right := 1
+func maxProfitS(prices []int) int {
     maxprofit := 0
-//  length := len(prices)
-    
-//     if length == 0{
-//         return maxprofit
-//     }
-    
-//     for right < length{
-//         if prices[left] < prices[right]{
-//             // check max profit
-//             profit := prices[right] - prices[left]
-//             if profit > maxprofit{
-//                 maxprofit = profit
-//             }
-//         }else{
-//             // since price[right] has lower selling price,
-//             // update left pointer to have right pointer index value
-//             left = right
-//         }
-//         // keep moving right pointer
-//         right += 1
-//     }
     
     // simplified
     minprice := prices[0]
@@ -49,4 +24,32 @@ func min(a,b int) int{
         return b
     }
     return a
+}
+
+
+func maxProfit(prices []int) int {
+    ////  init left, right, maxprofit, slice length
+    left := 0
+    right := 1
+    maxprofit := 0
+    length := len(prices)
+    
+    if length == 0{
+        return maxprofit
+    }
+    
+    for right < length{
+        if prices[left] < prices[right]{
+            // check max profit
+            maxprofit = max(maxprofit, prices[right] - prices[left])
+        }else{
+            // since price[right] has lower selling price,
+            // update left pointer to have right pointer index value
+            left = right
+        }
+        // keep moving right pointer
+        right += 1
+    }
+    
+    return maxprofit
 }
