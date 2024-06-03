5260eba7e5e2222bdf298affd841ef932e5c9c27
Quiyet Brul <qbrul0514@gmail.com>
2024-06-03 10:29:53 -0600

    Time: 15 ms (77.63%), Space: 26.7 MB (70.64%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,8 +1,25 @@
 class Solution {
 public:
     int majorityElement(std::vector<int>& nums) {
-        std::sort(nums.begin(), nums.end());
-        int n = nums.size();
-        return nums[n/2];
+        // linear sol
+        
+        int count = 1;
+        int answer = nums[0];
+        
+        for(int i = 1; i < nums.size(); i++){
+            if (nums[i] == answer) count++;
+            else count --;
+            if(count == 0){
+                answer = nums[i];
+                count = 1;
+            }
+        }
+        return answer;
+        
+        
+        //// nlogn
+        // std::sort(nums.begin(), nums.end());
+        // int n = nums.size();
+        // return nums[n/2];
     }
 };
