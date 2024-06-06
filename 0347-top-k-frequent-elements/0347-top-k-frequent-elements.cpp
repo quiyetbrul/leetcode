1cd614cfdddec8fc687b151dc9989f38af1a3845
Quiyet Brul <qbrul0514@gmail.com>
2024-06-05 21:04:44 -0600

    Update 0347-top-k-frequent-elements.cpp

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,42 +1,34 @@
 class Solution {
 public:
-    std::vector<int> topKFrequent(std::vector<int> &nums, int k) {
-    if (nums.size() <= 1) 
-        return nums;
-
-    std::unordered_map<int, int> numsMap;
-
-    // populating numsMap with nums values
-    for (const auto &num : nums) 
-        numsMap[num]++;
-
-    // Declare vector of pairs
-    std::vector<std::pair<int, int>> A;
-
-    //// found out about std::transform from G4G
-    // for (auto &it : numsMap) 
-    //     A.push_back(it);
-    //// literally not readable at ALL
-    //// ig plus points for learning an STL method but minus points for not code being readable???
-    std::transform(numsMap.begin(), numsMap.end(), std::back_inserter(A),
-        [](const std::pair<int, int>& pair) {
-            return pair;
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        int n = nums.size();
+        
+        // find freq of each nums[i]
+        unordered_map<int, int> m;
+        for (int i = 0; i < n; i++) {
+            m[nums[i]]++;
         }
-    );
-
-    // sort from G4G
-    auto cmp = [](const std::pair<int, int> &a, const std::pair<int, int> &b) {
-        return a.second > b.second; // Sort in descending order
-    };
-
-    std::sort(A.begin(), A.end(), cmp);
-
-    std::vector<int> result;
-    // Extract the k most frequent elements
-    for (int i = 0; i < k && i < A.size(); i++) {
-        result.push_back(A[i].first);
-    }
-
-    return result;
+        
+        // index is freq, nums[i] is element
+        // eg 1,1,1,2,2,2,3
+        // {3},{},{1,2},{},{},{},{}
+        vector<vector<int>> buckets(n + 1);
+        for (auto it = m.begin(); it != m.end(); it++) {
+            buckets[it->second].push_back(it->first);
+        }
+        
+        vector<int> result;
+        // iterate desc
+        for (int i = n; i >= 0; i--) {
+            if (result.size() >= k) {
+                break;
+            }
+            if (!buckets[i].empty()) {
+                // add all elements at buckets[i]
+                result.insert(result.end(), buckets[i].begin(), buckets[i].end());
+            }
+        }
+        
+        return result;
     }
 };
