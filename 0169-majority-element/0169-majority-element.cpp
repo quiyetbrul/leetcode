5d9b8f88cfbb04fbf2fc793418d48bb878b3c6bd
Quiyet Brul <qbrul0514@gmail.com>
2024-06-10 22:50:19 -0600

    Time: 11 ms (93.88%), Space: 26.8 MB (47.02%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
-    int majorityElement(std::vector<int>& nums) {
-        // linear sol
-        
+    int majorityElement(vector<int>& nums) {
         int count = 1;
         int answer = nums[0];
         
+        // For each element, it increments a counter
+        // if the element equals the current majority element, or decrements it otherwise.
+        // If the counter hits zero,
+        // the current element becomes the new candidate and the counter resets to 1.
         for(int i = 1; i < nums.size(); i++){
             if (nums[i] == answer) count++;
             else count --;
@@ -15,11 +17,5 @@ public:
             }
         }
         return answer;
-        
-        
-        //// nlogn
-        // std::sort(nums.begin(), nums.end());
-        // int n = nums.size();
-        // return nums[n/2];
     }
 };
