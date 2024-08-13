56814e1a29d7f22aab761b3e415bdff63efbdab5
Quiyet Brul <qbrul0514@gmail.com>
2024-08-13 15:51:06 -0600

    Time: 6 ms (90.33%), Space: 13.1 MB (68.79%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,6 +1,23 @@
 class Solution {
 public:
     int lengthOfLIS(std::vector<int>& nums) {
+
+    // just DP
+    // int n = nums.size();
+    // if (n == 0) {
+    //     return 0;
+    // }
+    // vector<int> L(n, 1);
+    // for (int i = 1; i < n; i++) {
+    //     for (int j = 0; j < i; j++) {
+    //         if (nums[j] < nums[i] && L[i] < 1 + L[j]) {
+    //             L[i] = 1 + L[j];
+    //         }
+    //     }
+    // }
+    // return *max_element(L.begin(), L.end());
+
+    // DP+BS
     std::vector<int> dp;
     for (int num : nums) {
         auto it = std::lower_bound(dp.begin(), dp.end(), num);
