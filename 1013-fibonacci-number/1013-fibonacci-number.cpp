bd0ef4abc2290e2b8596d6dca85238173c15d420
Quiyet Brul <qbrul0514@gmail.com>
2024-08-13 15:33:30 -0600

    Time: 2 ms (80.9%), Space: 7.4 MB (20.12%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -5,14 +5,13 @@ public:
         if(n==1)return 1;
 
         // DP
-        std::vector<int> fib;
-        fib.push_back(0);
-        fib.push_back(1);
-        
-        for (int i = 2; i <= n; i++)
-            fib.push_back(fib[i-1] + fib[i-2]);
-        
-        return fib[n];
+        int F[n + 1];
+        F[0] = 0;
+        F[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            F[i] = F[i - 1] + F[i - 2];
+        }
+        return F[n];
 
         // recursion
         // return fib(n-1) + fib(n-2);
