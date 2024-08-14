a0bf1c070564d6c8c2e3f175d61f1907855aa738
Quiyet Brul <qbrul0514@gmail.com>
2024-08-14 11:25:00 -0600

    Time: 35 ms (40.25%), Space: 3.6 MB (61.78%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.go
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.go
@@ -1,33 +1,33 @@
 func lengthOfLIS(nums []int) int {
     // BST
-    var dp []int
-    for _, num := range nums {
-        it := sort.Search(len(dp), func(i int) bool { return dp[i] >= num })
-        if it == len(dp) {
-            dp = append(dp, num)
-        } else {
-            dp[it] = num
-        }
-    }
-    return len(dp)
-
-    // DP
-    // n := len(nums)
-    // if n == 0{
-    //     return 0
-    // }
-    // L := make([]int, n)
-    // for i := range L {
-    //     L[i] = 1
-    // }
-    // for i := 1; i < n; i++{
-    //     for j := 0; j < i; j++{
-    //         if nums[j] < nums[i] && L[i] < 1 + L[j]{
-    //             L[i] = 1 + L[j]
-    //         }
+    // var dp []int
+    // for _, num := range nums {
+    //     it := sort.Search(len(dp), func(i int) bool { return dp[i] >= num })
+    //     if it == len(dp) {
+    //         dp = append(dp, num)
+    //     } else {
+    //         dp[it] = num
     //     }
     // }
-    // return maxElement(L);
+    // return len(dp)
+
+    // DP
+    n := len(nums)
+    if n == 0{
+        return 0
+    }
+    L := make([]int, n)
+    for i := range L {
+        L[i] = 1
+    }
+    for i := 1; i < n; i++{
+        for j := 0; j < i; j++{
+            if nums[j] < nums[i] && L[i] < 1 + L[j]{
+                L[i] = 1 + L[j]
+            }
+        }
+    }
+    return maxElement(L);
 }
 
 func maxElement(L []int) int {
