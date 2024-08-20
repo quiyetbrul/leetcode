7104c435d76abec52a99b99318d2537606441e61
Quiyet Brul <qbrul0514@gmail.com>
2024-08-20 15:22:20 -0600

    Time: 36 ms (9.25%), Space: 39.4 MB (48.81%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,43 +1,32 @@
 class Solution {
 public:
-std::vector<int> productExceptSelf(std::vector<int> nums) {
-    int n = nums.size();
-    std::vector<int> results(n, 1);
+    std::vector<int> productExceptSelf(std::vector<int> nums) {
+        int n = nums.size();
+        std::vector<int> results(n, 1);
+        
+        int leftProduct = 1;
+        for (int i = 0; i < n; i++) {
+            results[i] *= leftProduct;
+            leftProduct *= nums[i];
+        }
+        int rightProduct = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            results[i] *= rightProduct;
+            rightProduct *= nums[i];
+        }
+        return results;
+        
+        // OR
+
+        // int res = 1;
+        // for (int id = 0; id < n; id++) {
+        //     int res = 1;
+        //     for (int i = 0; i < n; i++)
+        //         if (i != id)
+        //             res *= nums[i];
+        //     results[id] = res;
+        // }
     
-    int leftProduct = 1;
-    for (int i = 0; i < n; i++) {
-        results[i] *= leftProduct;
-        leftProduct *= nums[i];
+        // return results;
     }
-
-    int rightProduct = 1;
-    for (int i = n - 1; i >= 0; i--) {
-        results[i] *= rightProduct;
-        rightProduct *= nums[i];
-    }
-
-    return results;
-    
-    //// OR
-
-//     int n = nums.size();
-//     std::vector<int> results(n, 1);
-//     int res = 1;
-
-//     for (int i = 0, id = 0; id < n; i++) {
-//         if (i == id) {
-//             i++;
-//         }
-//         if (i == n) {
-//             results[id] = res;
-//             res = 1;
-//             id++;
-//             i = 0;
-//         }
-//         if (id < n) {
-//             res *= nums[i];
-//         }
-//     }
-//     return results;
-}
 };
