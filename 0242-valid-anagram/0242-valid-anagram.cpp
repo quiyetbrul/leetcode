42cb58aab6daba97e6dd5703477372c3f8674fa8
Quiyet Brul <qbrul0514@gmail.com>
2024-08-20 11:55:32 -0600

    Time: 12 ms (30.03%), Space: 10.1 MB (11.83%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -3,10 +3,26 @@ public:
     bool isAnagram(string s, string t) {
         if(s.length() != t.length()) return false;
         
-        std::sort(s.begin(), s.end());
-        std::sort(t.begin(), t.end());
+        // std::sort(s.begin(), s.end());
+        // std::sort(t.begin(), t.end());
         
-        return s == t;
+        // return s == t;
+
+        // OR
+
+        std::unordered_map<char32_t, int>anagrams;
+
+        for(const auto &c : s)
+            anagrams[c]++;
+
+        for(const auto &c : t){
+            if(anagrams[c] && anagrams[c] > 0)
+                anagrams[c]--;
+            if(anagrams[c] == 0)
+                anagrams.erase(c);
+        }
+
+        return anagrams.empty();
         
     }
 };
