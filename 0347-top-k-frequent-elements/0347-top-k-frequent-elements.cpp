2b82bf12eebcd1b951c89c4294aa386387c178de
Quiyet Brul <qbrul0514@gmail.com>
2024-08-20 14:23:05 -0600

    Time: 14 ms (34.07%), Space: 19.3 MB (7.53%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -2,33 +2,35 @@ class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
         int n = nums.size();
-        
-        // find freq of each nums[i]
-        unordered_map<int, int> m;
-        for (int i = 0; i < n; i++) {
-            m[nums[i]]++;
+
+        if (n < 1){
+            return {};
         }
-        
+
+        std::unordered_map<int, int> m;
+
+        // element's freq
+        for(const auto& i : nums){
+            m[i]++;
+        }
+
         // index is freq, nums[i] is element
         // eg 1,1,1,2,2,2,3
         // {3},{},{1,2},{},{},{},{}
-        vector<vector<int>> buckets(n + 1);
-        for (auto it = m.begin(); it != m.end(); it++) {
-            buckets[it->second].push_back(it->first);
+        // eliminate sorting by
+        // making the index the frequency
+        std::vector<std::vector<int>> freq(n+1);
+        for(auto it = m.begin(); it != m.end(); it++){
+            freq[it->second].push_back(it->first);
         }
-        
-        vector<int> result;
-        // iterate desc
-        for (int i = n; i >= 0; i--) {
-            if (result.size() >= k) {
-                break;
-            }
-            if (!buckets[i].empty()) {
-                // add all elements at buckets[i]
-                result.insert(result.end(), buckets[i].begin(), buckets[i].end());
-            }
+
+        std::vector<int> result;
+        for(int i = n; i >= 0; i--){
+            if(freq[i].empty()) continue;
+            if(result.size() >= k) break;
+            result.insert(result.end(), freq[i].begin(), freq[i].end());
         }
-        
+
         return result;
     }
 };
