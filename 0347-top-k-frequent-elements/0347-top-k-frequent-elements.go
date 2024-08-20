82b87df9215193f2454d186de99995543cafedb8
Quiyet Brul <qbrul0514@gmail.com>
2024-08-20 11:49:21 -0600

    Time: 9 ms (69.95%), Space: 6.2 MB (43.8%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.go
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.go
@@ -1,25 +1,63 @@
+import "container/heap"
+
+type Item struct{
+    value int
+    priority int
+}
+
+type PriorityQueue []*Item
+
+func (pq PriorityQueue) Len() int { return len(pq) }
+
+func (pq PriorityQueue) Less(i, j int) bool {
+    // We want Pop to give us the highest, not lowest, priority so we use greater than here.
+    return pq[i].priority < pq[j].priority
+}
+
+func (pq PriorityQueue) Swap(i, j int) {
+    pq[i], pq[j] = pq[j], pq[i]
+}
+
+func (pq *PriorityQueue) Push(x interface{}) {
+    item := x.(*Item)
+    *pq = append(*pq, item)
+}
+
+func (pq *PriorityQueue) Pop() interface{} {
+    old := *pq
+    n := len(old)
+    item := old[n-1]
+    *pq = old[0 : n-1]
+    return item
+}
+
 func topKFrequent(nums []int, k int) (res []int) {
-	countMap := map[int]int{}
-	countSlice := make([][]int, len(nums)+1)
+    if len(nums) == 0 {
+        return []int{}
+    }
 
-	for _, num := range nums {
-		if count, ok := countMap[num]; ok {
-			countMap[num] = count + 1
-		} else {
-			countMap[num] = 1
-		}
-	}
+    // Step 1: Count the frequency of each element
+    freqMap := make(map[int]int)
+    for _, n := range nums {
+        freqMap[n]++
+    }
 
-	for num, count := range countMap {
-		countSlice[count] = append(countSlice[count], num)
-	}
+    // Step 2: Use a min-heap to keep track of the top k elements
+    pq := &PriorityQueue{}
+    heap.Init(pq)
 
-	for i := len(countSlice) - 1; i > 0; i-- {
-		res = append(res, countSlice[i]...)
-		if len(res) == k {
-			return
-		}
-	}
+    for value, priority := range freqMap {
+        heap.Push(pq, &Item{value: value, priority: priority})
+        if pq.Len() > k {
+            heap.Pop(pq)
+        }
+    }
 
-	return
+    // Step 3: Extract the top k elements from the heap
+    for pq.Len() > 0 {
+        item := heap.Pop(pq).(*Item)
+        res = append(res, item.value)
+    }
+
+    return res
 }
