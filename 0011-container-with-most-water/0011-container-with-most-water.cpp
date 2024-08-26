bce0c1bb96a06a9961708cc5e251e4b99f281700
Quiyet Brul <qbrul0514@gmail.com>
2024-08-26 17:06:41 -0600

    Time: 70 ms (13.98%), Space: 61.8 MB (22.08%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -2,23 +2,17 @@ class Solution {
 public:
     int maxArea(vector<int>& height) {
         int area = 0;
-        int h = 0;
-        int w = 0;
 
-        int start = 0;
-        int end = height.size() - 1;
+        int left = 0;
+        int right = height.size() - 1;
+        int currentArea = 0;
 
-        while(start < end){
-            h = std::min(height[start], height[end]);
-            w = end - start;
-
-            if (h*w > area)
-                area = h*w;
-            
-            height[start] < height[end] ? start++ : end--;
+        while(left < right){
+            currentArea = std::min(height[left], height[right]) * (right - left);
+            area = std::max(currentArea, area);
+            height[left] < height[right] ? left++ : right--;
         }
 
         return area;
-        
     }
 };
