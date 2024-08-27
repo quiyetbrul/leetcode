8c3579be8df001d82c6c1db7443613dda1f47011
Quiyet Brul <qbrul0514@gmail.com>
2024-08-26 23:42:48 -0600

    Time: 10 ms (65.89%), Space: 24.7 MB (34.68%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,12 +1,26 @@
 class Solution {
 public:
-    int trap(vector<int>& h) {
-        int l=0,r=h.size()-1,lmax=INT_MIN,rmax=INT_MIN,ans=0;
-        while(l<r){
-            lmax=max(lmax,h[l]);
-            rmax=max(rmax,h[r]);
-            ans+=(lmax<rmax)?lmax-h[l++]:rmax-h[r--];
+    int trap(vector<int>& height) {
+        int i = 0;
+        int j = height.size() - 1;
+        
+        int maxLeft = height[i];
+        int maxRight = height[j];
+        
+        int result = 0;
+        
+        while (i < j) {
+            if (maxLeft <= maxRight) {
+                i++;
+                maxLeft = max(maxLeft, height[i]);
+                result += maxLeft - height[i];
+            } else {
+                j--;
+                maxRight = max(maxRight, height[j]);
+                result += maxRight - height[j];
+            }
         }
-        return ans;
+        
+        return result;
     }
 };
