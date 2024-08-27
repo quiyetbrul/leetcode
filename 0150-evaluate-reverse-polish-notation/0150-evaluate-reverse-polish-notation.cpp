9f03298e4a757d59e712420d5154e662da714da3
Quiyet Brul <qbrul0514@gmail.com>
2024-08-27 12:00:10 -0600

    Time: 6 ms (76.95%), Space: 15.8 MB (17.67%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -1,30 +1,27 @@
 class Solution {
 public:
     int evalRPN(vector<string>& tokens) {
-        std::stack<int> st;
-        for(int i = 0; i < tokens.size(); i++){
-            if(tokens[i] == "+" || tokens[i] == "-" || tokens[i] == "*" || tokens[i] == "/") {
-                int num1 = st.top();
-                st.pop();
-                int num2 = st.top();
-                st.pop();
-                // switch statements don't work with strings.
-                // must be a char or a number
-                // hmm BUT str[i] is technically a char...
-                if(tokens[i] == "+") {
-                    st.push(num2 + num1);
-                }
-                else if(tokens[i] == "-") {
-                    st.push(num2 - num1);
-                }
-                else if(tokens[i] == "*") {
-                    st.push(num2 * num1);
-                }
-                else if(tokens[i] == "/") {
-                    st.push(num2 / num1);
-                }
-            } else st.push(stoi(tokens[i]));
+        std::stack<int> s;
+
+        for (const auto& i : tokens) {
+            if(i == "+"||i== "-"||i== "*"||i== "/"){
+                int num1 = s.top();
+                s.pop();
+                int num2 = s.top();
+                s.pop();
+
+                if (i == "+")
+                    s.push(num2 + num1);
+                if (i == "-")
+                    s.push(num2 - num1);
+                if (i == "*")
+                    s.push(num2 * num1);
+                if (i == "/")
+                    s.push(num2 / num1);
+            }
+            else s.push(std::stoi(i));
         }
-        return st.top();
+
+        return s.top();
     }
 };
