22936bf106bc903f6145cb96b766e07a2cc9f686
Quiyet Brul <qbrul0514@gmail.com>
2024-08-28 17:41:23 -0600

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0817-design-hashmap/0817-design-hashmap.cpp
+++ 0817-design-hashmap/0817-design-hashmap.cpp
@@ -1,24 +1,26 @@
-#include <vector>
-#include <utility> // for std::pair
-
 class MyHashMap {
-private:
-    std::vector<int> mp;
-
 public:
     MyHashMap() {
-        mp.resize(1000001, -1);
+        
     }
     
     void put(int key, int value) {
-        mp[key] = value;
+        
     }
     
     int get(int key) {
-        return mp[key] != -1 ? mp[key] : -1;
+        
     }
     
     void remove(int key) {
-        mp[key] = - 1;
+        
     }
 };
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap* obj = new MyHashMap();
+ * obj->put(key,value);
+ * int param_2 = obj->get(key);
+ * obj->remove(key);
+ */
