849b403382bf931af4f0df8364a43dc72e07352a
Quiyet Brul <qbrul0514@gmail.com>
2024-08-28 17:43:51 -0600

    Time: 0 ms (100%), Space: 7.9 MB (24.58%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,6 +1,20 @@
 class Solution {
 public:
-    bool isValid(string s) {
-        
+    bool isValid(string str) {
+        std::stack<char> s;
+
+        for(const auto& c : str){
+            if(c == '(' || c == '{' || c == '[')
+                s.push(c);
+            else if(s.empty() ||
+                   ( c == ')' && s.top() != '(') || 
+                    (c == '}' && s.top() != '{') || 
+                    (c == ']' && s.top() != '[' )
+                    )
+                return false;
+            else s.pop();
+        }
+
+        return s.empty();
     }
 };
