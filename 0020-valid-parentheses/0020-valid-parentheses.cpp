13c40a0112991b7eff958eb507f865280fc18d93
Quiyet Brul <qbrul0514@gmail.com>
2024-08-28 17:43:15 -0600

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,20 +1,6 @@
 class Solution {
 public:
-    bool isValid(string str) {
-        std::stack<char> s;
-
-        for(const auto& c : str){
-            if(c == '(' || c == '{' || c == '[')
-                s.push(c);
-            else if(s.empty() ||
-                   ( c == ')' && s.top() != '(') || 
-                    (c == '}' && s.top() != '{') || 
-                    (c == ']' && s.top() != '[' )
-                    )
-                return false;
-            else s.pop();
-        }
-
-        return s.empty();
+    bool isValid(string s) {
+        
     }
 };
