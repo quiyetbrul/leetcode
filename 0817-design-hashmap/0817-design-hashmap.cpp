ba3c8882660eb4ca39953a86b2ce05525ed0abd4
Quiyet Brul <qbrul0514@gmail.com>
2024-08-28 17:38:56 -0600

    Time: 98 ms (74.3%), Space: 217.7 MB (22.62%) - LeetHub

--- 0817-design-hashmap/0817-design-hashmap.cpp
+++ 0817-design-hashmap/0817-design-hashmap.cpp
@@ -3,28 +3,22 @@
 
 class MyHashMap {
 private:
-    std::vector<std::pair<bool, int>> mp;
+    std::vector<int> mp;
 
 public:
     MyHashMap() {
-        mp.resize(1000001, std::make_pair(false, -1));
+        mp.resize(1000001, -1);
     }
     
     void put(int key, int value) {
-        if (!mp[key].first)
-            mp[key].first = true;
-
-        mp[key].second = value;
+        mp[key] = value;
     }
     
     int get(int key) {
-        return !mp[key].first ? -1 : mp[key].second;
+        return mp[key] != -1 ? mp[key] : -1;
     }
     
     void remove(int key) {
-        if (mp[key].first) {
-            mp[key].first = false;
-            mp[key].second = -1;
-        }
+        mp[key] = - 1;
     }
 };
