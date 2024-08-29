6c13aa7edd603737b266ce7674243e6b010ac6b3
Quiyet Brul <qbrul0514@gmail.com>
2024-08-29 14:23:21 -0600

    Update 1095-two-city-scheduling.cpp

--- 1095-two-city-scheduling/1095-two-city-scheduling.cpp
+++ 1095-two-city-scheduling/1095-two-city-scheduling.cpp
@@ -5,6 +5,7 @@ public:
         int size = costs.size();
         int requiredAttendance = size / 2;
 
+        // O(n) space
         std::vector<std::vector<int>> diff;
 
         for (int i = 0; i < size; ++i) {
@@ -12,11 +13,12 @@ public:
             diff.push_back({costs[i][1] - costs[i][0], costs[i][0], costs[i][1]});
         }
 
+        // O(nlogn) time
         // Sort based on the difference
         std::sort(diff.begin(), diff.end(),
-                  [](const std::vector<int>& a, const std::vector<int>& b) {
-                      return a[0] < b[0];
-                  });
+                [](const std::vector<int>& a, const std::vector<int>& b) {
+                    return a[0] < b[0];
+                });
 
         // Iterate over the sorted diff vector
         // The first half will go to city A, the second half to city B
