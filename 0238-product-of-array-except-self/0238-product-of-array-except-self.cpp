6f319ec5414fe3bc244981fd922315cdd237ffc4
Quiyet Brul <qbrul0514@gmail.com>
2024-09-03 15:32:10 -0600

    Time: 19 ms (94.67%), Space: 38.4 MB (83.58%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,32 +1,24 @@
 class Solution {
 public:
-    std::vector<int> productExceptSelf(std::vector<int> nums) {
+    vector<int> productExceptSelf(vector<int>& nums) {
         int n = nums.size();
         std::vector<int> results(n, 1);
-        
-        int leftProduct = 1;
-        for (int i = 0; i < n; i++) {
-            results[i] *= leftProduct;
-            leftProduct *= nums[i];
-        }
-        int rightProduct = 1;
-        for (int i = n - 1; i >= 0; i--) {
-            results[i] *= rightProduct;
-            rightProduct *= nums[i];
-        }
-        return results;
-        
-        // OR
 
-        // int res = 1;
-        // for (int id = 0; id < n; id++) {
-        //     int res = 1;
-        //     for (int i = 0; i < n; i++)
-        //         if (i != id)
-        //             res *= nums[i];
-        //     results[id] = res;
-        // }
-    
-        // return results;
+        int leftProduct = 1;
+        int rightProduct = 1;
+        int left = 0;
+        int right = n - 1;
+
+        while (left < n && right >= 0) {
+            results[left] *= leftProduct;
+            leftProduct *= nums[left];
+            left++;
+
+            results[right] *= rightProduct;
+            rightProduct *= nums[right];
+            right--;
+        }
+
+        return results;
     }
 };
