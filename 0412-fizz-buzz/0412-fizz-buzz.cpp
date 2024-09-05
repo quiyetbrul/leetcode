256bf79d87c54cde91e80150ae4d40db8ed30acd
Quiyet Brul <qbrul0514@gmail.com>
2024-09-05 00:31:46 -0600

    Time: 0 ms (100%), Space: 11.4 MB (65.76%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        std::vector<std::string> results;
+
+        for(int i = 1; i <= n; i++){
+            if( i % 3 == 0 && i % 5 == 0) results.push_back("FizzBuzz");
+            else if(i % 3 == 0 ) results.push_back("Fizz");
+            else if(i % 5 == 0 ) results.push_back("Buzz");
+            else results.push_back(to_string(i));
+        }
+
+        return results;
+    }
+};
