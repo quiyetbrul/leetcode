65b36442a75511d956146cdf6f1c8bd840cf6432
Quiyet Brul <qbrul0514@gmail.com>
2024-09-13 01:47:31 -0600

    Time: 3 ms (56.56%), Space: 13 MB (51.38%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -1,30 +1,21 @@
 class Solution {
 public:
     int findMin(vector<int>& nums) {
-        int result = nums[0];
+        if (nums[0] < nums.back())
+            return nums[0];
+
         int left = 0;
         int right = nums.size() - 1;
-        while (left <= right){
 
-            // check if the array is already sorted
-            if (nums[left] < nums[right]){
-                result = std::min(result, nums[left]);
-                break;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid;
             }
-
-            // use mid element as potential min val
-            int mid = (left+right)/2;
-            result = std::min(result, nums[mid]);
-
-            // binary search
-            if (nums[mid] >= nums[left]) left = mid + 1;
-            else right = mid - 1;
         }
-        return result;
 
-        //// without using binary search
-        // for (int i = 0; i < nums.size() - 1; i++)
-        //     if (nums[i] > nums[i+1]) return nums[i+1];
-        // return nums[0];
+        return nums[left];
     }
 };
