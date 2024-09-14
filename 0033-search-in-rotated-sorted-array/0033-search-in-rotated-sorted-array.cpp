fe1929d5a62ff966d58b7ccdb643f27476c35ebb
Quiyet Brul <qbrul0514@gmail.com>
2024-09-14 00:18:45 -0600

    Update 0033-search-in-rotated-sorted-array.cpp

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -30,39 +30,40 @@ public:
 
     int search2(vector<int>& nums, int target) {
         int n = nums.size();
-        int left = 0;
-        int right = n - 1;
+        if (n == 0) return -1;
 
-        int minIndex;
+        // Step 1: Find the pivot (smallest element)
+        int left = 0, right = n - 1;
         while (left < right) {
             int mid = left + (right - left) / 2;
-            if (nums[mid] > nums[right])
-                minIndex = mid + 1;
-            else
-                minIndex = mid;
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
         }
+        int pivot = left;
 
-        if (minIndex == 0) {
-            left = 0;
-            right = n - 1;
-        } else if (nums[minIndex] >= nums[0] && target <= nums[minIndex - 1]) {
-            left = 0;
-            right = minIndex - 1;
+        // Step 2: Determine which subarray to search
+        left = 0, right = n - 1;
+        if (target >= nums[pivot] && target <= nums[right]) {
+            left = pivot;
         } else {
-            left = minIndex;
-            right = n - 1;
+            right = pivot - 1;
         }
 
+        // Step 3: Perform binary search in the selected subarray
         while (left <= right) {
             int mid = left + (right - left) / 2;
-
-            if (nums[mid] == target)
-                return target;
-            else if (nums[mid] > target)
-                right = mid - 1;
-            else
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
                 left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
         }
+
         return -1;
     }
 };
