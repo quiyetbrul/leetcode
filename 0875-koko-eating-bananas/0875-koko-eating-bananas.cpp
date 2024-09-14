c97b3d4c9a3e358f9409370944f253e4ce0d10d9
Quiyet Brul <qbrul0514@gmail.com>
2024-09-13 21:34:53 -0600

    koko update

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
     int minEatingSpeed(vector<int>& piles, int h) {
-        std::sort(piles.begin(), piles.end());
-        
-        int left = 1;
-        int right = piles.back();
-        
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-            int hours = findHours(piles, mid);
+        int min = 1;
+        int max = *std::max_element(piles.begin(), piles.end());
 
-            if (hours > h) left = mid + 1;
-            else right = mid;
+        while (min < max) {
+            int mid = min + (max - min) / 2;
+            if (kWorks(piles, mid, h))
+                max = mid;
+            else
+                min = mid + 1;
         }
-        return left;
+
+        return min;
     }
 
 private:
-    int findHours(const std::vector<int>& piles, int mid) {
-        int total = 0;
-        for (int pile : piles)
-            total += (pile + mid - 1) / mid;
-        return total;
+    bool kWorks(std::vector<int>& piles, int k, int h) {
+        int hours = 0;
+        for (const auto& i : piles)
+            hours += (i + k - 1) / k;
+        return hours <= h;
     }
-
 };
--- 0875-koko-eating-bananas/NOTES.md
+++ 0875-koko-eating-bananas/NOTES.md
@@ -1,1 +1,18 @@
-​
+# Thought Process
+
+- it's a binary search problem!
+- find min and max value needed to eat all bananas
+  - min: 1
+  - max: max(piles)
+- binary search for the minimum speed
+  - while min < max
+    - mid = (min + max) / 2
+    - if kWorks(mid, piles, h)
+      - max = mid
+    - else
+      - min = mid + 1
+- kWorks(mid, piles, h)
+  - hours = 0
+  - for p in piles
+    - hours += (p + mid - 1) / mid // ceil division 
+  - return hours <= h
