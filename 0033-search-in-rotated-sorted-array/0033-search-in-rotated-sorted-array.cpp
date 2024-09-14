22217d36da188e21f615fec858ada9c5dbd4db58
Quiyet Brul <qbrul0514@gmail.com>
2024-09-14 00:07:31 -0600

    Time: 0 ms (100%), Space: 14 MB (47.77%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -3,18 +3,65 @@ public:
     int search(vector<int>& nums, int target) {
         int left = 0;
         int right = nums.size() - 1;
-        while (left <= right){
-            int mid = (left+right)/2;
-            if(target==nums[mid]) return mid;
-            
-            // left sorted portion
-            if(nums[mid] >= nums[left])
-                if(target > nums[mid] || target < nums[left]) left = mid+1;
-                else right = mid-1;
-            // right sorted portion
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target)
+                return mid;
+
+            if (nums[left] <= nums[mid]) { // Left part is sorted
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else { // Right part is sorted
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    int search2(vector<int>& nums, int target) {
+        int n = nums.size();
+        int left = 0;
+        int right = n - 1;
+
+        int minIndex;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > nums[right])
+                minIndex = mid + 1;
             else
-                if(target < nums[mid] || target > nums[right]) right = mid-1;
-                else left = mid+1;
+                minIndex = mid;
+        }
+
+        if (minIndex == 0) {
+            left = 0;
+            right = n - 1;
+        } else if (nums[minIndex] >= nums[0] && target <= nums[minIndex - 1]) {
+            left = 0;
+            right = minIndex - 1;
+        } else {
+            left = minIndex;
+            right = n - 1;
+        }
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target)
+                return target;
+            else if (nums[mid] > target)
+                right = mid - 1;
+            else
+                left = mid + 1;
         }
         return -1;
     }
