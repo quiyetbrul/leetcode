3303335e45e082cdf34e1c0832d77f5a977c6ff5
Quiyet Brul <qbrul0514@gmail.com>
2024-09-13 23:13:25 -0600

    Time: 0 ms (100%), Space: 7.5 MB (54.68%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.cpp
+++ 0367-valid-perfect-square/0367-valid-perfect-square.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
     bool isPerfectSquare(int num) {
+        if (num == 1)
+            return true; // Special case for 1
+
         int left = 1;
-        int right = num;
+        int right = num / 2;
 
         while (left <= right) {
             int mid = left + (right - left) / 2;
             long long sq = static_cast<long long>(mid) * mid;
 
-            if (sq == num) return true;
-            else if (sq > num) right = mid - 1;
-            else left = mid + 1;
+            if (sq == num)
+                return true;
+            else if (sq > num)
+                right = mid - 1;
+            else
+                left = mid + 1;
         }
 
         return false;
