32cd5a395b54a825a23c7e169997525a45f15cc8
Quiyet Brul <qbrul0514@gmail.com>
2024-09-13 22:43:39 -0600

    Time: 111 ms (32.02%), Space: 84.8 MB (90.79%) - LeetHub

---
+++ 2503-longest-subarray-with-maximum-bitwise-and/2503-longest-subarray-with-maximum-bitwise-and.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums) {
+        int maxElement = *max_element(nums.begin(), nums.end());
+        int maxLength = 0;
+        int currentLength = 0;
+
+        for (int num : nums) {
+            if (num == maxElement) {
+                currentLength++;
+                maxLength = max(maxLength, currentLength);
+            } else {
+                currentLength = 0;
+            }
+        }
+
+        return maxLength;
+    }
+};
