1009ddfeb65d6c165d7f7c7a9d7224c35e00dc8f
Quiyet Brul <qbrul0514@gmail.com>
2024-09-25 14:56:59 -0600

    Time: 0 ms (100%), Space: 8.7 MB (80.97%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,32 +1,39 @@
 class Solution {
-public:
-    bool checkInclusion(string s1, string s2) {
-        if (s1.length() > s2.length()) {
-            return false;
-        }
-
-        std::vector<int> a1(26, 0);
-        std::vector<int> a2(26, 0);
-
-        for (const auto& c : s1) {
-            a1[c - 'a'] += 1;
-        }
-
-        int left = 0;
-        int right = 0;
-
-        while (right < s2.length()) {
-            a2[s2[right] - 'a'] += 1;
-
-            if (right - left + 1 == s1.length()) {
-                if (a1 == a2) return true;
-                a2[s2[left] - 'a'] -= 1;
-                left++;
-            }
-
-            right++;
-        }
-
-        return false;
+ public:
+  bool checkInclusion(string s1, string s2) {
+    int m = s1.size();
+    int n = s2.size();
+    if (m > n) {
+      return false;
     }
+
+    vector<int> count(26);
+    for (int i = 0; i < m; i++) {
+      count[s1[i] - 'a']++;
+      count[s2[i] - 'a']--;
+    }
+    if (isPermutation(count)) {
+      return true;
+    }
+
+    for (int i = m; i < n; i++) {
+      count[s2[i] - 'a']--;
+      count[s2[i - m] - 'a']++;
+      if (isPermutation(count)) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+ private:
+  bool isPermutation(vector<int>& count) {
+    for (int i = 0; i < 26; i++) {
+      if (count[i] != 0) {
+        return false;
+      }
+    }
+    return true;
+  }
 };
