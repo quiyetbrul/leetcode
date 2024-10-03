a12b4d16c5ad120e7861f3e0ef79538ca255387e
Quiyet Brul <qbrul0514@gmail.com>
2024-10-02 23:09:17 -0600

    Time: 10 ms (48.79%), Space: 19.6 MB (29.42%) - LeetHub

--- 1556-make-two-arrays-equal-by-reversing-subarrays/1556-make-two-arrays-equal-by-reversing-subarrays.cpp
+++ 1556-make-two-arrays-equal-by-reversing-subarrays/1556-make-two-arrays-equal-by-reversing-subarrays.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     bool canBeEqual(vector<int>& target, vector<int>& arr) {
+        if (target.size() != arr.size()) {
+            return false;
+        }
         std::unordered_map<int, int> t;
         for (const auto& i : target) {
             t[i]++;
