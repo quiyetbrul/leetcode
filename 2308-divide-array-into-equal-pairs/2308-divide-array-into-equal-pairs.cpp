e03056b2c37db43028b29cc21132b23c2c6bec59
Quiyet Brul <qbrul0514@gmail.com>
2024-10-03 00:11:37 -0600

    Time: 14 ms (33.91%), Space: 18.4 MB (42.93%) - LeetHub

--- 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.cpp
+++ 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.cpp
@@ -1,9 +1,22 @@
 class Solution {
 public:
     bool divideArray(vector<int>& nums) {
-        std::sort(nums.begin(), nums.end());
-        for(int i = 1; i < nums.size(); i+=2){
-            if(nums[i]!=nums[i-1]) return false;
+        // std::sort(nums.begin(), nums.end());
+        // for (int i = 1; i < nums.size(); i += 2) {
+        //     if (nums[i] != nums[i - 1])
+        //         return false;
+        // }
+
+        // OR without sorting using a map
+
+        std::unordered_map<int, int> mp;
+        for (const auto& i : nums) {
+            mp[i]++;
+        }
+
+        for (const auto& [k, v] : mp) {
+            if (v % 2 != 0)
+                return false;
         }
 
         return true;
