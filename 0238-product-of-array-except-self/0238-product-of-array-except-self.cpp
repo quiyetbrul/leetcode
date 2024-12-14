621602c7d275c44807fdedea66377636dd5200d3
Quiyet Brul <qbrul0514@gmail.com>
2024-12-14 13:45:19 -0700

    Time: 0 ms (100%), Space: 40.1 MB (51.23%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -5,16 +5,22 @@ public:
         std::vector<int> results(n, 1);
 
         int leftProduct = 1;
+        // [1,2,3,4]
+        // since initial leftProduct is 1,
+        // the first element is essentially ignored
+        // and every ith element is multiplied by
+        // the running product of the previous elements
+        // the ith element is still ignored since all elements are init to 1
+        for(int i = 0; i < n; i++){
+            results[i] *= leftProduct;
+            leftProduct *= nums[i];
+        }
+
+
         int rightProduct = 1;
-        int left = 0;
-        int right = n - 1;
-
-        while (left < n && right >= 0) {
-            results[left] *= leftProduct;
-            leftProduct *= nums[left++];
-
-            results[right] *= rightProduct;
-            rightProduct *= nums[right--];
+        for(int i = n - 1; i >=0; i--){
+            results[i] *= rightProduct;
+            rightProduct *= nums[i];
         }
 
         return results;
