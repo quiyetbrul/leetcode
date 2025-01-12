797e8ff3f3f19e630d24625bbf2b04594221745d
Quiyet Brul <qbrul0514@gmail.com>
2025-01-11 18:08:35 -0700

    Time: 76 ms (6.32%), Space: 77.1 MB (7.49%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,35 +1,7 @@
 class Solution {
 public:
-    
-bool containsDuplicate(std::vector<int> &nums) {
-  std::set<int> s;
-  for (const auto &i : nums) {
-    s.insert(i);
-  }
-
-  return s.size() != nums.size();
-}
-    
-bool containsDuplicateUnorderedMap(std::vector<int> &nums) {
-  if (nums.size() > 1) {
-    std::unordered_map<int, int> numsMap;
-    for (const auto &i : nums) {
-      if (numsMap[i])
-        return true;
-      numsMap[i] = 1;
+    bool containsDuplicate(vector<int>& nums) {
+        std::set<int> s (nums.begin(), nums.end());
+        return s.size() != nums.size();
     }
-  }
-  return false;
-}
-    
-bool containsDuplicateSort(std::vector<int> &nums) {
-  sort(nums.begin(), nums.end());
-  int n = nums.size();
-  for (int i = 1; i < n; i++) {
-    if (nums[i] == nums[i - 1])
-      return true;
-  }
-  return false;
-}
-
 };
