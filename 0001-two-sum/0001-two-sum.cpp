a7dc26d9e37af5ac85a85eb4320e285960dd36ed
Quiyet Brul <qbrul0514@gmail.com>
2025-01-11 18:44:24 -0700

    Time: 4 ms (62.79%), Space: 14.8 MB (39.09%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> map;
-        for(size_t i = 0; i < nums.size(); ++i) {
-            map.emplace(std::make_pair(nums.at(i),i));
-        }
-        for(size_t i = 0; i < nums.size(); ++i) {
-            const auto mit = map.find(target - nums.at(i));
-            if(mit != map.end() && (int)i != mit->second) {
-                return {(int)i, mit->second};
+        std::unordered_map<int,int> m;
+
+        for(int i = 0; i < nums.size(); i++){
+            int comp = target - nums[i];
+            if(m.contains(comp)){
+                return {m[comp], i};
             }
+            m[nums[i]] = i;
         }
-        return vector<int>();
+
+        return {};
     }
 };
