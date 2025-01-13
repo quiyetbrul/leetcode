4c2d648ea8e01de598fe134b4d16bd4e195ffc0d
Quiyet Brul <qbrul0514@gmail.com>
2025-01-13 16:43:38 -0700

    Time: 78 ms (54.36%), Space: 81.3 MB (16.61%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,27 +1,22 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        if(nums.size() <= 1) return nums.size();
-
-        std::unordered_set<int> numsSet(nums.begin(), nums.end());
-
-        int maxLength = 0;
+        std::unordered_set<int> us(nums.begin(), nums.end());
+        int result = 0;
         int currentNum = 0;
-        int currentLength = 0;
+        int counter = 0;
 
-        // since vector was turned into a set,
-        // we can just check (either increasingly/decreasingly)
-        // if the next/previous integer is present
-        for(const auto& i : numsSet){
-            currentNum = i;
-            if (!numsSet.contains(i - 1)) {
-                currentLength = 1;
-                while (numsSet.contains(++currentNum))
-                    currentLength++;
+        for(const auto& n : us){
+            currentNum = n;
+            if(!us.contains(currentNum-1)){
+                counter = 0;
+                while(us.contains(currentNum++)){
+                    counter++;
+                }
+                result = std::max(result, counter);
             }
-
-            maxLength = std::max(currentLength, maxLength);
         }
-        return maxLength;
+
+        return result;
     }
 };
