3b8c3e38079f68e4b0f8313f2d1185035ebfa6dd
Quiyet Brul <qbrul0514@gmail.com>
2025-01-12 17:29:25 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,36 +1,24 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        int n = nums.size();
+        if(nums.empty()) return{};
 
-        if (n < 1){
-            return {};
+        std::unordered_map<int,int> m;
+        for(const auto& n : nums){
+            m[n]++;
         }
 
-        std::unordered_map<int, int> m;
-
-        // element's freq
-        for(const auto& i : nums){
-            m[i]++;
+        std::vector<std::vector<int>> freq(nums.size()+1);
+        for(const auto& [k,v] : m){
+            freq[k].push_back(v);
         }
 
-        // index is freq, nums[i] is element
-        // eg 1,1,1,2,2,2,3
-        // {3},{},{1,2},{},{},{},{}
-        // eliminate sorting by
-        // making the index the frequency
-        std::vector<std::vector<int>> freq(n+1);
-        for(auto it = m.begin(); it != m.end(); it++){
-            freq[it->second].push_back(it->first);
+        std::vector<int> results;
+        for(int i = nums.size(); i >= 0; i--){
+            if(!freq[i].empty() && results.size() < k)
+                results.insert(results.end(), freq[i].begin(), freq[i].end());
         }
 
-        std::vector<int> result;
-        for(int i = n; i >= 0; i--){
-            if(freq[i].empty()) continue;
-            if(result.size() >= k) break;
-            result.insert(result.end(), freq[i].begin(), freq[i].end());
-        }
-
-        return result;
+        return results;
     }
 };
