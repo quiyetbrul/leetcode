def01ac28c2a36111c4414147bc4ddf6ce0b8e1b
Quiyet Brul <qbrul0514@gmail.com>
2025-01-13 23:15:40 -0700

    Time: 0 ms (100%), Space: 8.8 MB (29.58%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     bool isValid(string str) {
-        if(str.length() % 2 != 0) return false;
         std::stack<char> s;
 
         for(const auto& c : str){
-            if(c == '(' || c == '{' || c == '[')
+            if(c == '(' || c == '[' || c == '{'){
                 s.push(c);
-            else if(s.empty() ||
-                    (c == ')' && s.top() != '(') || 
-                    (c == '}' && s.top() != '{') || 
-                    (c == ']' && s.top() != '[' )
-                    )
+            }else if(s.empty() ||
+                    (c == ')' && s.top() != '(') ||
+                    (c == '}' && s.top() != '{') ||
+                    (c == ']' && s.top() != '[')){
                 return false;
-            else s.pop();
+            }else {
+                s.pop();
+            }
         }
 
         return s.empty();
