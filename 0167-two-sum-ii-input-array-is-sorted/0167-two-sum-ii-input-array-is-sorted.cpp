7214923902c18bf8f89156c976a945d223dd2fb6
Quiyet Brul <qbrul0514@gmail.com>
2025-01-13 22:24:45 -0700

    Time: 2 ms (12.19%), Space: 19.6 MB (27.32%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -1,21 +1,36 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
-        int l = 0;
-        int r = numbers.size() - 1;
+        // int l = 0;
+        // int r = numbers.size() - 1;
 
-        while(l <= r){
-            int sum = numbers[l] + numbers[r];
-            if(sum > target){
-                r--;
-                continue;
-            }
-            if(sum < target){
-                l++;
-                continue;
-            }
-            if(sum == target){
-                return {l+1, r+1};
+        // while(l <= r){
+        //     int sum = numbers[l] + numbers[r];
+        //     if(sum > target){
+        //         r--;
+        //         continue;
+        //     }
+        //     if(sum < target){
+        //         l++;
+        //         continue;
+        //     }
+        //     if(sum == target){
+        //         return {l+1, r+1};
+        //     }
+        // }
+
+        // return {};
+
+        std::unordered_map<int, int> m;
+
+        for(int i = 0; i < numbers.size(); i++){
+            m[numbers[i]] = i;
+        }
+
+        for(int i = 0; i < numbers.size(); i++){
+            int comp = target - numbers[i];
+            if(m.contains(comp)){
+                return {i + 1, m[comp] + 1};
             }
         }
 
