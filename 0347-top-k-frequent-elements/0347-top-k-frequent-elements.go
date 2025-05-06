5d8bd06789f96151a66c3ed1d1300494caa065aa
Quiyet Brul <qbrul0514@gmail.com>
2025-05-06 13:04:08 -0600

    Time: 0 ms (100%), Space: 8.1 MB (28.74%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.go
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.go
@@ -1,90 +1,50 @@
-import "container/heap"
-
-type Item struct{
-    value int
-    priority int
-}
-
-type PriorityQueue []*Item
-
-func (pq PriorityQueue) Len() int { return len(pq) }
-
-func (pq PriorityQueue) Less(i, j int) bool {
-    // We want Pop to give us the highest, not lowest, priority so we use greater than here.
-    return pq[i].priority < pq[j].priority
-}
-
-func (pq PriorityQueue) Swap(i, j int) {
-    pq[i], pq[j] = pq[j], pq[i]
-}
-
-func (pq *PriorityQueue) Push(x interface{}) {
-    item := x.(*Item)
-    *pq = append(*pq, item)
-}
-
-func (pq *PriorityQueue) Pop() interface{} {
-    old := *pq
-    n := len(old)
-    item := old[n-1]
-    *pq = old[0 : n-1]
-    return item
-}
-
-func topKFrequent(nums []int, k int) (res []int) {
-    if len(nums) == 0 {
-        return []int{}
+func topKFrequent(nums []int, k int) []int {
+    if len(nums) < k{
+        return nil
     }
 
-    // Step 1: Count the frequency of each element
-    freqMap := make(map[int]int)
-    for _, n := range nums {
-        freqMap[n]++
+    // create a map
+    // key will be the element from given nums
+    // value will be the frequency of said element
+    // [1,1,1,2,2,3]
+    // m[1], 3
+    // m[2], 2
+    // m[3], 1
+    m := make(map[int]int)
+    for _, n := range nums{
+        m[n]++
     }
 
-    // Step 2: Use a min-heap to keep track of the top k elements
-    pq := &PriorityQueue{}
-    heap.Init(pq)
+    // create 2D array
+    // row will be the value from map
+    // col will be the key from map
+    /*
+        {
+            {},
+            {3},
+            {2},
+            {1},
+            {},
+            {}
+        }
+    */
+    s := make([][]int, len(nums) + 1)
+    for k, v := range m{
+        s[v] = append(s[v], k)
+    }
 
-    for value, priority := range freqMap {
-        heap.Push(pq, &Item{value: value, priority: priority})
-        if pq.Len() > k {
-            heap.Pop(pq)
+    result := []int{}
+    // iterate backwards
+    for i := len(s) - 1; i >= 0 && len(result) < k; i-- {
+        // only append value at row[i] if it's not empty
+        if len(s[i]) > 0 {
+            result = append(result, s[i]...)
+            // Trim if we added too many
+            if len(result) > k {
+                result = result[:k]
+            }
         }
     }
 
-    // Step 3: Extract the top k elements from the heap
-    for pq.Len() > 0 {
-        item := heap.Pop(pq).(*Item)
-        res = append(res, item.value)
-    }
-
-    return res
-
-
-    // OR a simpler approach
-
-    // countMap := map[int]int{}
-	// countSlice := make([][]int, len(nums)+1)
-
-	// for _, num := range nums {
-	// 	if count, ok := countMap[num]; ok {
-	// 		countMap[num] = count + 1
-	// 	} else {
-	// 		countMap[num] = 1
-	// 	}
-	// }
-
-	// for num, count := range countMap {
-	// 	countSlice[count] = append(countSlice[count], num)
-	// }
-
-	// for i := len(countSlice) - 1; i > 0; i-- {
-	// 	res = append(res, countSlice[i]...)
-	// 	if len(res) == k {
-	// 		return
-	// 	}
-	// }
-
-	// return
+    return result;
 }
