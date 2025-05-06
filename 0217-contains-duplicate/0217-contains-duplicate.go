18118827c580aca2e2f7144be97a1adcc62ba4a7
Quiyet Brul <qbrul0514@gmail.com>
2025-05-06 08:48:24 -0600

    Time: 39 ms (5.45%), Space: 12.4 MB (32.94%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.go
+++ 0217-contains-duplicate/0217-contains-duplicate.go
@@ -1,9 +1,13 @@
 func containsDuplicate(nums []int) bool {
+
+    // there's no built-in suppport for Set data structure.
+    // instead, utilize Map with the data as key and bool for value;
+    // value can also be an empty struct{}
     s := make(map[int]bool)
 
     for _, num := range nums{
-        s[num] = true
+        s[num] = true;
     }
 
-    return len(nums) != len(s)
+    return len(nums) != len(s);
 }
