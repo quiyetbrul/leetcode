263739c9556ab930a14632e5015ca19a91d660cb
Quiyet Brul <qbrul0514@gmail.com>
2025-05-06 09:00:01 -0600

    Time: 6 ms (34.77%), Space: 4.7 MB (71.99%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.go
+++ 0242-valid-anagram/0242-valid-anagram.go
@@ -1,22 +1,21 @@
 func isAnagram(s string, t string) bool {
     if len(s) != len(t){
-        return false
+        return false;
     }
 
-    m := make(map[rune]int)
+    word := make(map[rune]int)
 
-    for _, c := range s{
-        m[c]++
+    for _, r := range s{
+        word[r]++
     }
 
-    for _, c := range t{
-        if m[c] <= 0{
-            return false
+    for _, r := range t{
+        if word[r] <= 0{
+            return false;
         }
 
-        m[c]--
+        word[r]--
     }
 
     return true
-    
 }
