901790e51491aeb858cbac3754b70ab6efae31aa
Quiyet Brul <qbrul0514@gmail.com>
2025-05-21 18:11:22 -0600

    Time: 0 ms (100%), Space: 4.1 MB (88.06%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.go
+++ 0020-valid-parentheses/0020-valid-parentheses.go
@@ -1,25 +1,23 @@
 func isValid(s string) bool {
     stack := []rune{}
-    for _, bracket := range s{
-        if bracket == '(' || bracket == '[' || bracket == '{'{
-            stack = append(stack, bracket)
+
+    for _, char := range s{
+        if char == '(' || char == '{' || char == '['{
+            stack = append(stack, char)
         }else{
             if len(stack) == 0{
                 return false
             }
             last := stack[len(stack)-1]
             stack = stack[:len(stack)-1]
-            comb := string(last) + string(bracket) 
-            if Valid(comb){
+            c := string(last) + string(char)
+            if c == "()" || c == "{}" || c == "[]"{
                 continue
             }else{
                 return false
             }
         }
     }
-    return len(stack)==0
-}
 
-func Valid(s string) bool {
-    return s == "()" || s=="[]" || s=="{}"
+    return len(stack) == 0
 }
