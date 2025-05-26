511e966618626199b9634c8fbc3801b793e5cca7
Quiyet Brul <qbrul0514@gmail.com>
2025-05-25 22:59:17 -0600

    Time: 0 ms (100%), Space: 4.5 MB (23.98%) - LeetHub

---
+++ 0206-reverse-linked-list/0206-reverse-linked-list.go
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func reverseList(head *ListNode) *ListNode {
+    if head == nil || head.Next == nil{
+        return head
+    }
+
+    curr:= head
+    var prev *ListNode = nil
+
+    for curr != nil{
+        nxt := curr.Next
+        curr.Next = prev
+        prev = curr
+        curr = nxt
+    }
+
+    return prev
+}
