0f71f82116e23aa9823f986bfbcd8c1805c63053
Quiyet Brul <qbrul0514@gmail.com>
2025-05-27 02:21:38 -0600

    Time: 71 ms (75.63%), Space: 98.6 MB (41.3%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -1,23 +1,11 @@
 class Solution {
 public:
-    // bool containsNearbyDuplicate(std::vector<int>& nums, int k) {       
-    //     std::unordered_map<int, int> m; // key element, value index
-    //     for (int i = 0; i < nums.size(); i++) {
-    //         auto it = m.find(nums[i]);
-    //         if (it != m.end() && i - it->second <= k) return true;
-    //         m[nums[i]] = i;
-    //     }
-    //     return false;
-    // }
-    
-    bool containsNearbyDuplicate(std::vector<int>& nums, int k) {
-        std::unordered_set<int> m;
-        for (int i = 0; i < nums.size(); ++i) {
-            if (i > k) // If m size exceeds 'k', remove the oldest element from the m
-                m.erase(nums[i - k - 1]);
-            if (m.find(nums[i]) != m.end()) // Check if the current element already exists in the m
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        std::unordered_map<int, int> m; // key = {element}, val = {index}
+        for(int i = 0; i < nums.size(); i++){
+            if(m.contains(nums[i]) && abs(m[nums[i]] - i) <= k)
                 return true;
-            m.insert(nums[i]); // Add the current element to the m
+            m[nums[i]] = i;
         }
         return false;
     }
