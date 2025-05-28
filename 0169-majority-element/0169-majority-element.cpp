22c73a859626155e3fb112bc8242b12362d32a6b
Quiyet Brul <qbrul0514@gmail.com>
2025-05-28 16:31:14 -0600

    Time: 0 ms (100%), Space: 28.1 MB (84%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
+        int majority = nums[0];
         int count = 1;
-        int answer = nums[0];
-        
-        // For each element, it increments a counter
-        // if the element equals the current majority element, or decrements it otherwise.
-        // If the counter hits zero,
-        // the current element becomes the new candidate and the counter resets to 1.
-        for(int i = 1; i < nums.size(); i++){
-            if (nums[i] == answer) count++;
-            else count --;
-            if(count == 0){
-                answer = nums[i];
+
+        for(const auto &i : nums){
+            if(majority == i){
+                count++;
+            }else{
+                count--;
+            }
+
+            if(count <= 0){
                 count = 1;
+                majority = i;
             }
         }
-        return answer;
+
+        return majority;
     }
 };
