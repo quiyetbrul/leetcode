b9fafc42cf81e41452e972174911ac4588e6c7ac
Quiyet Brul <qbrul0514@gmail.com>
2025-11-17 22:34:10 -0700

    Time: 0 ms (100%), Space: 4.6 MB (33.33%) - LeetHub

---
+++ 0717-1-bit-and-2-bit-characters/0717-1-bit-and-2-bit-characters.go
@@ -0,0 +1,12 @@
+func isOneBitCharacter(bits []int) bool {
+     i := 0 
+     n := len(bits)
+     for i < n-1 {
+        if bits[i] == 1{
+            i += 2
+        }else {
+            i++
+        }
+     }
+     return i == n-1
+}
