c2e76f1a40cea170479a5e600af5f042f74bd5c1
Quiyet Brul <qbrul0514@gmail.com>
2025-11-19 04:36:50 -0700

    Time: 0 ms (100%), Space: 5.9 MB (35.48%) - LeetHub

--- 2274-keep-multiplying-found-values-by-two/2274-keep-multiplying-found-values-by-two.go
+++ 2274-keep-multiplying-found-values-by-two/2274-keep-multiplying-found-values-by-two.go
@@ -1,19 +1,28 @@
 func findFinalValue(nums []int, original int) int {
-	m := make(map[int]bool)
+	m := make(map[int]bool, len(nums))
 
 	for _, num := range nums {
 		m[num] = true
 	}
 
-	result := original
-
-	for {
-		if _, ok := m[result]; ok {
-			result *= 2
-		} else {
-			break
-		}
+	for m[original] {
+		original *= 2
 	}
 
-	return result
+	return original
+}
+
+func findFinalValueBits(nums []int, k int) int {
+    bits := 0
+    for _, num := range nums {
+        if num % k != 0 {
+            continue
+        }
+        n := num / k
+        if n & (n - 1) == 0 {
+            bits |= n
+        }
+    }
+    d := bits + 1
+    return k * (d & -d)
 }
