ed6404f256a08a14b0c8ea2c9d1ebb4aedb28126
Quiyet Brul <qbrul0514@gmail.com>
2025-11-22 01:30:18 -0700

    Time: 0 ms (100%), Space: 8.7 MB (55.61%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.go
@@ -0,0 +1,18 @@
+func search(nums []int, target int) int {
+	lo := 0
+	hi := len(nums) - 1
+
+	for lo <= hi {
+		mid := lo + (hi - lo) / 2
+		if nums[mid] == target {
+			return mid
+		}
+		if nums[mid] > target {
+			hi = mid - 1
+		} else {
+			lo = mid + 1
+		}
+	}
+
+	return -1
+}
